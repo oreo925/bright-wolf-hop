bdf54361bacd90da46f75c19796877b7c556d15f
oreo925 <abdulwahab925@rocketmail.com>
2025-12-12 00:53:56 +0500

    final change

--- backend/main.py
+++ backend/main.py
@@ -42,3 +42,11 @@ app.include_router(notifications_router.router, prefix="/api/v1/notifications", 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000
+    )
